599ea46db9c7e8b3606fd5680649d5e87a642e9c
sparth292 <sparthsalunke@gmail.com>
2025-09-12 09:22:17 +0530

    ZXing scanner added

--- AadharParser.java
+++ AadharParser.java
@@ -8,6 +8,8 @@ import javax.imageio.ImageIO;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
@@ -23,6 +25,8 @@ public class AadharParser {
         File aadharFile = new File("aadharSample.jpeg");
         BufferedImage bufferedImage = ImageIO.read(aadharFile);
 
+        LuminanceSource src = new BufferedImageLuminanceSource(bufferedImage);
+    
         String xmlText = "<?xml version=\"1.0\" encoding=\"utf-8\"?> <printletterbarcodedata uid=\"436108442955\" name=\"parth shashikant salunke\" gender=\"m\" yob=\"2008\" gname=\"shraddha shashikant salunke\" co=\"s/o: shashikant salunke\" house=\"406/b vaishnav sadan\" street=\"s.b pawar marg\" loc=\"currey road\" vtc=\"delisle road\" po=\"delisle road\" dist=\"mumbai\" subdist=\"mumbai\" state=\"maharashtra\" pc=\"400013\" dob=\"23/02/2008\"/>";
 
         // Parse XML string using an xml Parser
