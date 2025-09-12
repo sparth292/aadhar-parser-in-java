d215145d5fe3fc0b02b6f479cf34fa093889fa76
sparth292 <sparthsalunke@gmail.com>
2025-09-12 09:01:47 +0530

    QR Scanning initial push

--- AadharParser.java
+++ AadharParser.java
@@ -13,6 +13,10 @@ import org.w3c.dom.Node;
 public class AadharParser {
     
     public static void main(String[] args) throws Exception {
+        
+        // Image Scanner Integration
+
+
         String xmlText = "<?xml version=\"1.0\" encoding=\"utf-8\"?> <printletterbarcodedata uid=\"436108442955\" name=\"parth shashikant salunke\" gender=\"m\" yob=\"2008\" gname=\"shraddha shashikant salunke\" co=\"s/o: shashikant salunke\" house=\"406/b vaishnav sadan\" street=\"s.b pawar marg\" loc=\"currey road\" vtc=\"delisle road\" po=\"delisle road\" dist=\"mumbai\" subdist=\"mumbai\" state=\"maharashtra\" pc=\"400013\" dob=\"23/02/2008\"/>";
 
         // Parse XML string using an xml Parser
